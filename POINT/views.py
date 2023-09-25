58883ed8456b8e7b0ef09d8222423035ce361f56
Mariana Torres <mariandreatorres@gmail.com>
2023-09-25 11:58:44 -0300

    version 1 con diccionario

--- POINT/views.py
+++ POINT/views.py
@@ -12,6 +12,13 @@ def hoy(request):
 	return HttpResponse(f"Hoy es {fecha}.") 
 
 def pantalla_1(request):  ###prueba
+	nombre_p = "Mariana"
+	apellido_p = "Torres"
+	fecha_nac_p = "27/09/1971"
+	especialidad_p = "Ortodoncista"
+	
+	diccionario = {"nombre_prof":nombre_p, "apellido_prof":apellido_p, "fecha_nac_prof":fecha_nac_p, 
+					"especialidad_prof": especialidad_p}
 	
 	html_1 = open("C:/Users/cti0716/Python/PreEntrega3_Mariana_Torres/POINT/POINT/plantillas/principal.html")
 	plantilla = Template(html_1.read())
@@ -20,4 +27,16 @@ def pantalla_1(request):  ###prueba
 	documento_1 = plantilla.render(miContexto_1)
 	return HttpResponse(documento_1)
 
+class Cliente:
+
+    def __init__(self, id_cliente, nombre, apellido, mail, cuit, razon_social, gerencia_asignada):
+        self.id_cliente = id_cliente
+        self.nombre = nombre
+        self.apellido = apellido
+        self.mail = mail
+        self.cuit = cuit
+        self.razon_social = razon_social
+        self.gerencia_asignada = gerencia_asignada
+    
+    
 
