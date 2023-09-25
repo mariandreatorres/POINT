d676b71833e703fad37aa01536799be79afff90d
Mariana Torres <mariandreatorres@gmail.com>
2023-09-25 14:36:34 -0300

    version 3 haciendo ajustes

--- AppTurnos/models.py
+++ AppTurnos/models.py
@@ -1,31 +1,30 @@
 from django.db import models
+import datetime
 
 class Datos_profesionales(models.Model):
-    id_profesional = models.IntegerField
-    nombre = models.CharField(max_length=60)
-    apellido = models.CharField(max_length=60)
-    mail = models.CharField(max_length=60)
-    cuit = models.CharField(max_length=10)
-    razon_social = models.CharField(max_length=60)
-    especialidad = models.CharField(max_length=60)
-    # Create your models here.
-    """def __str__(self):
-        return f"Bienvenido {self.nombre}, {self.apellido} \n Razón Social: {self.razon_social} \n ID profesional {self.id_profesional} \n"
-    
-    def muestrainfo(self):
-        mensaje = f"Profesional:  {self.nombre} {self.apellido} \n ID Cliente {self.id_profesional} \n enviamos su factura de compra a: {self.mail}"
-        print(mensaje)"""
+    id_profesional = models.AutoField(primary_key=True)
+    nombre = models.CharField(default="",max_length=60)
+    apellido = models.CharField(default="",max_length=60)
+    mail = models.CharField(default="",max_length=60)
+    cuit = models.CharField(default="",max_length=10)
+    razon_social = models.CharField(default="",max_length=60)
+    especialidad = models.CharField(default="",max_length=60)
 
 class Horarios_profesionales(models.Model):
-    id_profesional = models.IntegerField
-    dia_semana = models.CharField(max_length=10)
-    hora_inicio = models.CharField(max_length=10)
-    hora_fin = models.CharField(max_length=10)
-       
-#    # Create your models here.
-#    def __str__(self):
-#       return f"Horarios {self.id_profesional}, \n Atiende los días {self.dia_semana} Desde: {self.hora_inicio} Hasta: {self.hora_fin} \n"
-#    
-#    def muestrainfo(self):
-#        mensaje = f"Horarios {self.id_profesional}, \n Atiende los días {self.dia_semana} Desde: {self.hora_inicio} Hasta: {self.hora_fin} \n"
-#        print(mensaje)
+    id_profesional = models.ForeignKey
+    dia_semana = models.CharField(default="",max_length=10)
+    hora_inicio = models.CharField(default="",max_length=10)
+    hora_fin = models.CharField(default="",max_length=10)
+
+class meses(models.Model):
+    periodo = models.CharField(default="202301", max_length=10)
+    id_profesional = models.ForeignKey
+    fecha = models.DateField(default=datetime.date.today)
+    hora = models.DateTimeField(default=datetime.time.today)   
+
+class pacientes(models.Model):
+    id_pacientes = models.AutoField(primary_key=True)
+    nombre = models.CharField(default="",max_length=60)
+    apellido = models.CharField(default="",max_length=60)
+    obra_social = models.CharField(default="",max_length=60)
+
