28603f3488ef86f78872eeef6675d740b78a287b
Mariana Torres <mariandreatorres@gmail.com>
2023-09-26 11:25:46 -0300

    continua mejorando la version

--- AppTurnos/models.py
+++ AppTurnos/models.py
@@ -1,30 +1,30 @@
 from django.db import models
-import datetime
+from datetime import datetime
 
-class Datos_profesionales(models.Model):
+class DatosProfesionales(models.Model):
     id_profesional = models.AutoField(primary_key=True)
-    nombre = models.CharField(default="",max_length=60)
-    apellido = models.CharField(default="",max_length=60)
-    mail = models.CharField(default="",max_length=60)
-    cuit = models.CharField(default="",max_length=10)
-    razon_social = models.CharField(default="",max_length=60)
-    especialidad = models.CharField(default="",max_length=60)
+    nombre = models.CharField(default="", max_length=60)
+    apellido = models.CharField(default="", max_length=60)
+    mail = models.CharField(default="", max_length=60)
+    cuit = models.CharField(default="", max_length=10)
+    razon_social = models.CharField(default="", max_length=60)
+    especialidad = models.CharField(default="", max_length=60)
 
-class Horarios_profesionales(models.Model):
-    id_profesional = models.ForeignKey
-    dia_semana = models.CharField(default="",max_length=10)
-    hora_inicio = models.CharField(default="",max_length=10)
-    hora_fin = models.CharField(default="",max_length=10)
+class HorariosProfesionales(models.Model):
+    id_profesional = models.ForeignKey(DatosProfesionales, on_delete=models.CASCADE)
+    dia_semana = models.CharField(default="", max_length=10)
+    hora_inicio = models.CharField(default="", max_length=10)
+    hora_fin = models.CharField(default="", max_length=10)
 
 class Meses(models.Model):
     periodo = models.CharField(default="202301", max_length=10)
-    id_profesional = models.ForeignKey
-    fecha = models.DateField(default=datetime.date.today)
-    hora =models.DateTimeField(default=datetime.datetime.now) 
+    id_profesional = models.ForeignKey(DatosProfesionales, on_delete=models.CASCADE)
+    fecha = models.DateField(default=datetime.today)
+    hora = models.DateTimeField(default=datetime.now)
 
 class Pacientes(models.Model):
-    id_pacientes = models.AutoField(primary_key=True)
-    nombre = models.CharField(default="",max_length=60)
-    apellido = models.CharField(default="",max_length=60)
-    obra_social = models.CharField(default="",max_length=60)
+    id_paciente = models.AutoField(primary_key=True)
+    nombre = models.CharField(default="", max_length=60)
+    apellido = models.CharField(default="", max_length=60)
+    obra_social = models.CharField(default="", max_length=60)
 
